71b6da156936ee94cbaf03c1a2963db802e887ad
gwachukwu <gwachukwuelebeke@gmail.com>
2024-07-26 22:52:08 -0400

    Update security

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,19 +2,28 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { appConfig } from "../../config";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const { apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId } = appConfig
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDKuyPdCtao9dO9yXc-FPOZtRzSs-kKPkA",
-  authDomain: "culturefex-7a63f.firebaseapp.com",
-  projectId: "culturefex-7a63f",
-  storageBucket: "culturefex-7a63f.appspot.com",
-  messagingSenderId: "260846266791",
-  appId: "1:260846266791:web:ae97c3a1c6f94635d9ae82",
-  measurementId: "G-FWSB8N5QK1"
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId
 };
 
 // Initialize Firebase
@@ -22,4 +31,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const auth = getAuth(app)
-export {app,auth}
+export { app, auth }
